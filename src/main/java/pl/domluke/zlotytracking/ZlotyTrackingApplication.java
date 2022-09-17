1d3dcb224f842667588a021cade98f41887483c7
DomLuke81 <d.kochanski.onoszko@gmail.com>
2022-09-17 14:10:33 +0200

    Start files

---
+++ src/main/java/pl/domluke/zlotytracking/ZlotyTrackingApplication.java
@@ -0,0 +1,13 @@
+package pl.domluke.zlotytracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZlotyTrackingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZlotyTrackingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/domluke/zlotytracking/ZlotyTrackingApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.domluke.zlotytracking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZlotyTrackingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
