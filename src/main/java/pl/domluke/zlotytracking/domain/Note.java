f29877fc37b35c06cd89c43eadb2885d5c6eaa49
DomLuke81 <d.kochanski.onoszko@gmail.com>
2022-09-23 23:16:57 +0200

    Note / NoteSpot - add Entities

--- src/main/java/pl/domluke/zlotytracking/domain/Note.java
+++ src/main/java/pl/domluke/zlotytracking/domain/Note.java
@@ -1,13 +1,60 @@
 package pl.domluke.zlotytracking.domain;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import java.util.Set;
 
 @Entity
-@Table(name = "notes")
+@Table(name = "notes",
+        uniqueConstraints = @UniqueConstraint(columnNames = {"note_type_id", "serial_number"}))
 public class Note {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-//    private
+    @Column(name = "serial_number", nullable = false, columnDefinition = "CHAR", length = 9)
+    @NotNull
+    @Pattern(regexp = "[A-Z]{2}\\d{7}")
+    private String serialNumber;
+
+    @ManyToOne
+    @JoinColumn(name = "note_type_id", nullable = false)
+    @NotNull
+    private NoteType emisja;
+
+    @OneToMany(fetch = FetchType.EAGER, mappedBy = "note")
+    private Set<NoteSpot> spots;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getSerialNumber() {
+        return serialNumber;
+    }
+
+    public void setSerialNumber(String serialNumber) {
+        this.serialNumber = serialNumber;
+    }
+
+    public NoteType getEmisja() {
+        return emisja;
+    }
+
+    public void setEmisja(NoteType emisja) {
+        this.emisja = emisja;
+    }
+
+    public Set<NoteSpot> getSpots() {
+        return spots;
+    }
+
+    public void setSpots(Set<NoteSpot> spots) {
+        this.spots = spots;
+    }
 }
---
+++ src/main/java/pl/domluke/zlotytracking/domain/NoteSpot.java
@@ -0,0 +1,85 @@
+package pl.domluke.zlotytracking.domain;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.time.ZonedDateTime;
+
+@Entity
+@Table(name = "note_spot")
+public class NoteSpot {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    @NotNull
+    private Note note;
+
+    @ManyToOne
+    @JoinColumn(name = "zip_code", nullable = false)
+    @NotNull
+    private LocationZipCode zipCode;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    @NotNull
+    private User user;
+
+    @Column(name = "spot_time", nullable = false)
+    @NotNull
+    private ZonedDateTime spotTime;
+
+    @Column
+    @Size(max = 255)
+    private String description;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Note getNote() {
+        return note;
+    }
+
+    public void setNote(Note note) {
+        this.note = note;
+    }
+
+    public LocationZipCode getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(LocationZipCode zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public ZonedDateTime getSpotTime() {
+        return spotTime;
+    }
+
+    public void setSpotTime(ZonedDateTime spotTime) {
+        this.spotTime = spotTime;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
