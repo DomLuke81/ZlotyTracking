feec5905937f4e259077f02b7c84ee537371c55d
DomLuke81 <d.kochanski.onoszko@gmail.com>
2022-10-05 23:33:03 +0200

    map with spots - fix showing map with one point

--- src/main/webapp/js/mapShowDistance.js
+++ src/main/webapp/js/mapShowDistance.js
@@ -1,15 +1,26 @@
+const trasaTd = document.querySelectorAll(".trasa");
+let map;
+
 function initMap() {
-    const directionsService = new google.maps.DirectionsService();
-    const directionsRenderer = new google.maps.DirectionsRenderer();
-    const map = new google.maps.Map(document.getElementById("map"));
-    calculateAndDisplayRoute(directionsService, directionsRenderer);
-    directionsRenderer.setMap(map);
+    if (trasaTd.length > 1) {
+        const directionsService = new google.maps.DirectionsService();
+        const directionsRenderer = new google.maps.DirectionsRenderer();
+        map = new google.maps.Map(document.getElementById("map"));
+        calculateAndDisplayRoute(directionsService, directionsRenderer);
+        directionsRenderer.setMap(map);
+    } else {
+        map = new google.maps.Map(document.getElementById("map"), {
+            zoom: 7,
+            mapTypeControl: false,
+        });
+        geocode({ address: trasaTd[0].innerHTML});
+    }
+
 }
 
 
 function calculateAndDisplayRoute(directionsService, directionsRenderer) {
     let waypts = [];
-    const trasaTd = document.querySelectorAll(".trasa");
 
     const start = trasaTd[0].innerHTML;
     const end = trasaTd[trasaTd.length - 1].innerHTML;
@@ -34,10 +45,28 @@ function calculateAndDisplayRoute(directionsService, directionsRenderer) {
             // For each route, display summary information.
             trasaTd[0].innerHTML = "";
             for (let i = 0; i < route.legs.length; i++) {
-                trasaTd[i+1].innerHTML = route.legs[i].distance.text;
+                trasaTd[i + 1].innerHTML = route.legs[i].distance.text;
             }
         })
         .catch((e) => window.alert("Directions request failed due to " + status));
+}
+
+function geocode(request) {
+    const geocoder = new google.maps.Geocoder();
+    const marker = new google.maps.Marker({map,});
+    geocoder
+        .geocode(request)
+        .then((result) => {
+            const { results } = result;
+
+            map.setCenter(results[0].geometry.location);
+            marker.setPosition(results[0].geometry.location);
+            marker.setMap(map);
+            return results;
+        })
+        .catch((e) => {
+            alert("Geocode was not successful for the following reason: " + e);
+        });
 }
 
 window.initMap = initMap;
